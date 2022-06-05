3111b720c9fa06885f29d8fa458df950cd199072
Maite Ladaria <74008091+MLS535@users.noreply.github.com>
2022-06-05 20:53:43 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <ButtonIncrease />
+</template>
+
+<script>
+import ButtonIncrease from "./components/ButtonIncrease.vue";
+export default {
+  name: "App",
+  components: {
+    ButtonIncrease,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ButtonIncrease.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>{{ counter }}</div>
+  <button @click="incrementNumber">Increase</button>
+  <button @click="reloadCounter">reload the counter</button>
+</template>
+<script>
+export default {
+  name: "ButtonIncrease",
+
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    incrementNumber: function () {
+      this.counter++;
+    },
+    reloadCounter: function () {
+      this.counter = 0;
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
